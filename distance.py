151fad32498ee88643affa8bc225f49c5f74feb3
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2024-11-07 17:20:59 +0100

    added forumla 2 to calculate distance

--- distance.py
+++ distance.py
@@ -9,16 +9,18 @@ class APTracker():
         self.wifiInterface = wifiInterface
     def fetch_APs(self):
 
-        result = subprocess.run("iwlist wlp0s20f3 scanning", shell=True, stdout=subprocess.PIPE,
+        result = subprocess.run(f"iwlist {self.wifiInterface}  scanning", shell=True, stdout=subprocess.PIPE,
                                  stderr=subprocess.STDOUT)
         out = result.stdout.decode("utf-8", errors="ignore")
+
         l1 = out.split("Cell")
 
         result1 = subprocess.run("nmcli dev wifi list", shell=True, stdout=subprocess.PIPE,
                                  stderr=subprocess.STDOUT)
         out1 = result1.stdout.decode("utf-8", errors="ignore")
         l2 = out1.split("\n")
-        print(l2)
+
+
 
         if len(l1)  > 1 and len(l2) > 1:
             l1 = l1[1::]
@@ -28,6 +30,7 @@ class APTracker():
                 AP=dict()
                 AP["BSSID"], AP["Frequency"], AP["SignalLevel"], AP["ESSID"]=m[0][0], m[0][1], m[0][2], m[0][3]
                 self.APs[AP["BSSID"]] = AP
+
             for i in l2:
                 m1 = re.findall('\*? *([A-F0-9]*:[A-F0-9]*:[A-F0-9]*:[A-F0-9]*:[A-F0-9]*:[A-F0-9]*).* ([0-9]*) Mbit/s.*',i,re.DOTALL)
                 if len(m1) > 0:
@@ -35,7 +38,7 @@ class APTracker():
                     if bssid in self.APs.keys():
                         self.APs[bssid]["Rate"]=rate
                         self.APs[bssid]["Distance"] = self.distance(self.APs[bssid]["SignalLevel"], self.APs[bssid]["Frequency"], rate)
-
+                        self.APs[bssid]["Distance2"]=self.distance2(int(self.APs[bssid]["SignalLevel"]))
     def format_AP(self,ap):
         ch=f"ESSID: {ap['ESSID']}\n"+"\n".join(["   "+str(i)+": "+str(j) for i,j in ap.items() if i!="ESSID"])
         ch=ch+'\n____________________'
@@ -49,7 +52,12 @@ class APTracker():
         fm = int(p) - (-154+10*log10(int(rate)*10**6))
         print(p, fm)
         return 10**((p0-0*fm-int(p)-10*n*log10(float(f)*1000)+30*n-32.44)/(10*n))
+    def distance2(self,p,n=2):
+        measured_power = -44
+        return 10**((measured_power-p)/(10*n))
 
-apt = APTracker()
+# ahmed idani interface : wlo1
+#haythem krid interface : wlp0s20f3
+apt = APTracker("wlo1")
 apt.fetch_APs()
 apt.show_APs()
